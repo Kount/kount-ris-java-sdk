fef35fe950fa13c3f2723b0805bba4099aa36e01
Boyan Kukushev <boyan.kukushev@gmail.com>
2017-08-22 16:04:32 +0300

    KS-191: base85 test updated

--- kount-ris-sdk/src/test/java/com/kount/ris/util/Base85EncodeDecodeTest.java
+++ kount-ris-sdk/src/test/java/com/kount/ris/util/Base85EncodeDecodeTest.java
@@ -10,25 +10,18 @@ import com.github.fzakaria.ascii85.Ascii85;
 
 public class Base85EncodeDecodeTest {
 
+	private static final String PLAIN_TEXT = "This is sample text for testing purposes.";
+	private static final String ENCODED_TEXT = "<+oue+DGm>F(&p)Ch4`2AU&;>AoD]4FCfN8Bl7Q+E-62?Df]K2/c";
+	
 	@Test
 	public void testEncode() throws UnsupportedEncodingException {
-		
-		final String plain = "This is sample text for testing purposes.";
-		final String expected = "<+oue+DGm>F(&p)Ch4`2AU&;>AoD]4FCfN8Bl7Q+E-62?Df]K2/c";
-		
-		String encoded = Ascii85.encode(plain.getBytes("UTF-8"));
-		
-		assertEquals("Encoded value is not the same as the expected", expected, encoded);
+		String encoded = Ascii85.encode(PLAIN_TEXT.getBytes("UTF-8"));
+		assertEquals("Encoded value is not the same as the expected", ENCODED_TEXT, encoded);
 	}
 
 	@Test
 	public void testDecode() throws UnsupportedEncodingException {
-		
-		final String encoded = "<+oue+DGm>F(&p)Ch4`2AU&;>AoD]4FCfN8Bl7Q+E-62?Df]K2/c";
-		final String expected = "This is sample text for testing purposes.";
-		
-		String decoded = new String(Ascii85.decode(encoded), "UTF-8");
-		
-		assertEquals("Decoded value is not the same as the expected", expected, decoded);
+		String decoded = new String(Ascii85.decode(ENCODED_TEXT), "UTF-8");
+		assertEquals("Decoded value is not the same as the expected", PLAIN_TEXT, decoded);
 	}
 }
