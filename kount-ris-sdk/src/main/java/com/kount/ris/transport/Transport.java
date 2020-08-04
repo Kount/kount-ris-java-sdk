9eb5fe1a21045d6dacb3c07a629b04c388658882
sanjeev.kumar <sanjeev.kumar@kount.com>
2020-08-04 23:38:50 +0530

    Added Transport.java

--- kount-ris-sdk/src/main/java/com/kount/ris/transport/Transport.java
+++ kount-ris-sdk/src/main/java/com/kount/ris/transport/Transport.java
@@ -1,18 +1,23 @@
 package com.kount.ris.transport;
 
 import com.kount.ris.util.RisTransportException;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
  * RIS data transport interface.
  * </p>
- * Provides basic and utility methods for configuring HTTP connections for communication with RIS.
+ * Provides basic and utility methods for configuring HTTP connections for
+ * communication with RIS.
  * 
  * @author Kount &lt;custserv@kount.com&gt;
  * @version $Id$
@@ -20,70 +25,66 @@ import java.util.Map;
  */
 public abstract class Transport {
 
+	private static Object String;
+
 	/**
 	 * Send transaction data to RIS.
 	 * 
-	 * @throws RisTransportException
-	 *             RIS transport exception
-	 * @param params
-	 *            Map of data to send
+	 * @throws RisTransportException RIS transport exception
+	 * @param params Map of data to send
 	 * @return Reader for character stream returned by RIS
 	 */
 	public abstract Reader send(Map<String, String> params) throws RisTransportException;
-	
+
 	/**
 	 * Ris server endpoint url.
 	 */
 	protected String risServerUrl;
-	
+
 	/**
-	 * Ris connection establishment timeout, a timeout of zero is interpreted as
-	 * an infinite timeout.
+	 * Ris connection establishment timeout, a timeout of zero is interpreted as an
+	 * infinite timeout.
 	 */
 	protected int connectTimeout;
 
 	/**
-	 * Ris connection read timeout, a timeout of zero is interpreted as an
-	 * infinite timeout.
+	 * Ris connection read timeout, a timeout of zero is interpreted as an infinite
+	 * timeout.
 	 */
 	protected int readTimeout;
 
 	/**
 	 * Set the RIS server url.
 	 * 
-	 * @param url
-	 *            Url
+	 * @param url Url
 	 */
 	public void setRisServerUrl(String url) {
 		this.risServerUrl = url;
 	}
-	
+
 	/**
-	 * Set the transports connection timeout. See
-	 * http://docs.oracle.com/javase/7
+	 * Set the transports connection timeout. See http://docs.oracle.com/javase/7
 	 * /docs/api/java/net/URLConnection.html#setConnectTimeout(int)
 	 * 
-	 * @param timeout
-	 *            Timeout in milliseconds
+	 * @param timeout Timeout in milliseconds
 	 */
 	public void setConnectTimeout(int timeout) {
 		this.connectTimeout = timeout;
 	}
 
 	/**
-	 * Set the transports connection read timeout.
-	 * http://docs.oracle.com/javase/
+	 * Set the transports connection read timeout. http://docs.oracle.com/javase/
 	 * 7/docs/api/java/net/URLConnection.html#setReadTimeout(int)
 	 * 
-	 * @param timeout
-	 *            Timeout in milliseconds
+	 * @param timeout Timeout in milliseconds
 	 */
 	public void setReadTimeout(int timeout) {
 		this.readTimeout = timeout;
 	}
-	
+
 	/**
-	 * Serializes the given map of request parameters to the provided {@link OutputStream}.
+	 * Serializes the given map of request parameters to the provided
+	 * {@link OutputStream}.
 	 * </p>
 	 * All values are URL-encoded with UTF-8.
 	 * 
@@ -91,7 +92,8 @@ public abstract class Transport {
 	 * @param params
 	 * @throws IOException
 	 */
-	protected static void writeParametersToOutput(OutputStreamWriter out, Map<String, String> params) throws IOException {
+	protected static void writeParametersToOutput(OutputStreamWriter out, Map<String, String> params)
+			throws IOException {
 		for (String key : params.keySet()) {
 			String value = params.get(key);
 			out.write(key);
@@ -99,5 +101,14 @@ public abstract class Transport {
 			out.write(URLEncoder.encode(null == value ? "" : value, "UTF-8"));
 			out.write('&');
 		}
+	}
+
+	protected List<NameValuePair> convertToNameValuePair(Map<String, String> params) {
+		List<NameValuePair> nvpList = new ArrayList<>(params.size());
+
+		for (Map.Entry<String, String> entry : params.entrySet()) {
+			nvpList.add(new BasicNameValuePair(entry.getKey(), entry.getValue()));
+		}
+		return nvpList;
 	}
 }
