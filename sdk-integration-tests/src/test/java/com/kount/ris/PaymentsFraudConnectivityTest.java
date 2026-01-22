c51203a7914efbce78913ba588776841e01b3f52
Greg Tyler <daniel.tyler@equifax.com>
2026-01-22 10:49:59 -0700

    Adding custom bearer token capabilities. Using this functionality is strongly discouraged and will result in warnings.

--- sdk-integration-tests/src/test/java/com/kount/ris/PaymentsFraudConnectivityTest.java
+++ sdk-integration-tests/src/test/java/com/kount/ris/PaymentsFraudConnectivityTest.java
@@ -1,18 +1,29 @@
 package com.kount.ris;
 
-import com.kount.ris.util.Address;
-import com.kount.ris.util.RisException;
-import com.kount.ris.util.TestConfiguration;
-import com.kount.ris.util.Utilities;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kount.ris.transport.BearerAuthResponse;
+import com.kount.ris.util.*;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.entity.UrlEncodedFormEntity;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 
 import javax.naming.ConfigurationException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
+import static com.kount.ris.transport.HttpApiTransport.PF_AUTH_HEADER;
+import static com.kount.ris.transport.HttpApiTransport.readAllInput;
+import static com.kount.ris.transport.Transport.convertToNameValuePair;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class PaymentsFraudConnectivityTest {
@@ -63,7 +74,8 @@ public class PaymentsFraudConnectivityTest {
         assertNotEquals("", response.getAuto());
     }
 
-    @Test void testForceCharset() throws UnsupportedEncodingException, RisException {
+    @Test
+    void testForceCharset() throws UnsupportedEncodingException, RisException {
         logger.debug("running test forcing charset for payments fraud");
 
         Inquiry inq = getInquiry(merchantId);
@@ -77,6 +89,78 @@ public class PaymentsFraudConnectivityTest {
         inq.setBillingAddress(address);
 
         Response response = utf8Client.process(inq);
+        logger.trace(response.toString());
+        assertNotEquals("E", response.getAuto());
+        assertNotEquals("", response.getAuto());
+    }
+
+    @Test
+    void testCustomBearerToken() throws RisException, UnsupportedEncodingException, MalformedURLException, ConfigurationException {
+        logger.debug("running test for custom bearer token");
+        URL serverUrl = new URL(TestConfiguration.getRisURL());
+
+        KountRisClient customClient = new KountRisClient(
+                serverUrl,
+                TestConfiguration.getRisAPIKey(),
+                true,
+                TestConfiguration.getPfApiKey(),
+                TestConfiguration.getPfClientId(),
+                TestConfiguration.getPfApiEndpoint(),
+                TestConfiguration.getPfAuthEndpoint()
+        );
+
+        BearerAuthResponse bearer;
+        HttpPost httpPost = new HttpPost(TestConfiguration.getPfAuthEndpoint());
+        httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded");
+        httpPost.addHeader(PF_AUTH_HEADER, "Basic " + TestConfiguration.getPfApiKey());
+        Map<String, String> params = new HashMap<>();
+        params.put("grant_type", "client_credentials");
+        params.put("scope", "k1_integration_api");
+
+        httpPost.setEntity(new UrlEncodedFormEntity(convertToNameValuePair(params)));
+
+        try (CloseableHttpClient httpClient = HttpClientBuilder
+                .create()
+                .build()) {
+
+            try (CloseableHttpResponse httpResponse = httpClient.execute(httpPost);
+                 Reader reader = new InputStreamReader(readAllInput(httpResponse.getEntity()))
+            ) {
+                if (httpResponse.getCode() < 400) {
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    BearerAuthResponse authResponse;
+                    //convert json string to object
+                    authResponse = objectMapper.readValue(reader, BearerAuthResponse.class);
+                    bearer = authResponse;
+                    bearer.getExpiresAt();
+
+                } else {
+                    String message = "Error fetching auth token: received " + httpResponse.getCode() + " " + httpResponse.getReasonPhrase();
+                    logger.error(message);
+                    throw new RisTransportException("An error occurred while reading the auth token response: " + message);
+                }
+            }
+        } catch (Exception ioe) {
+            throw new RisTransportException("An error occurred while getting the auth token", ioe);
+        }
+
+        BearerAuthResponse initialToken = customClient.getCustomBearerResponse();
+
+        customClient.setCustomBearerResponse(bearer);
+
+        if (customClient.getCustomBearerResponse() == null) {
+            throw new RisTransportException("token should be initialized");
+        }
+
+        BearerAuthResponse setToken = customClient.getCustomBearerResponse();
+
+        if (initialToken.getExpiresAt() == setToken.getExpiresAt() || initialToken.getAccessToken().equals(setToken.getAccessToken())) {
+            throw new RisTransportException("token should not be the same as the initial value");
+        }
+
+        Inquiry inq = getInquiry(merchantId);
+
+        Response response = customClient.process(inq);
         logger.trace(response.toString());
         assertNotEquals("E", response.getAuto());
         assertNotEquals("", response.getAuto());
